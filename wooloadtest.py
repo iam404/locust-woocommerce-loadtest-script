ad95302ca95cd2718cf3252516a15bc70fce7ebc
rtCamp <sys+jenkins@rtcamp.com>
2015-12-24 06:50:31 -0600

    random product added

--- wooloadtest.py
+++ wooloadtest.py
@@ -13,13 +13,14 @@ class UserBehavior(TaskSet):
         from splinter import Browser
 
         with Browser('phantomjs') as browser:   	  
+	    foo = ['70', '37', '53', '31', '56', '50' , '19', '73', '76', '93', '99', '47', '34', '79', '60', '15', '96', '90', '87', '83', '67']
 	# Visit URL
-            url = WebsiteUser.host + "shop/?add-to-cart=70"
+            url = WebsiteUser.host + "shop/?add-to-cart=" + random.choice(foo)
             browser.visit(url)
 	    url = WebsiteUser.host + "checkout/"
             browser.visit(url)
  #           browser.fill('billing_email', 'hello@dfdf.dfdfdfdf')
-    # Find and click the 'search' button
+    # Find and click the 'Place Order' button
             button = browser.find_by_name('woocommerce_checkout_place_order')
     # Interact with elements
             button.click()
